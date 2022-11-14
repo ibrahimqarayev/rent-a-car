599925b5df65fa6260ed5377b54e8682268e590b
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-14 16:46:32 +0400

    BrandsController

---
+++ src/main/java/com/rent/webApi/controllers/BrandsController.java
@@ -0,0 +1,36 @@
+package com.rent.webApi.controllers;
+
+
+
+import com.rent.bussiness.abstracts.BrandService;
+import com.rent.entities.concretes.Brand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/brands")
+public class BrandsController {
+
+    private BrandService brandService;
+
+    @Autowired
+    public BrandsController(BrandService brandService) {
+        this.brandService = brandService;
+    }
+
+    @GetMapping
+    public List<Brand> getAll() {
+     return brandService.getAll();
+    }
+}
+
+
+
+
+
+
