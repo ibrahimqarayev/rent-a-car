99c98c6b19afed9f96f2062f34635f96679098e1
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-14 16:46:12 +0400

    BrandManager

--- src/main/java/com/rent/bussiness/concretes/BrandManager.java
+++ src/main/java/com/rent/bussiness/concretes/BrandManager.java
@@ -3,12 +3,17 @@ package com.rent.bussiness.concretes;
 import com.rent.bussiness.abstracts.BrandService;
 import com.rent.dataAccess.abstracts.BrandRepository;
 import com.rent.entities.concretes.Brand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 
+@Service
 public class BrandManager implements BrandService {
     private BrandRepository brandRepository;
 
+    @Autowired
     public BrandManager(BrandRepository brandRepository) {
         this.brandRepository = brandRepository;
     }
