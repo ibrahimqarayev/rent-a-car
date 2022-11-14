558bed5515e5e9b748e95094042d448a49498bfb
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-14 16:09:50 +0400

    BrandManager

--- src/main/java/com/rent/bussiness/concretes/BrandManager.java
+++ src/main/java/com/rent/bussiness/concretes/BrandManager.java
@@ -1,4 +1,20 @@
 package com.rent.bussiness.concretes;
 
-public class BrandManager {
+import com.rent.bussiness.abstracts.BrandService;
+import com.rent.dataAccess.abstracts.BrandRepository;
+import com.rent.entities.concretes.Brand;
+
+import java.util.List;
+
+public class BrandManager implements BrandService {
+    private BrandRepository brandRepository;
+
+    public BrandManager(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+
+    @Override
+    public List<Brand> getAll() {
+        return brandRepository.getAll();
+    }
 }
