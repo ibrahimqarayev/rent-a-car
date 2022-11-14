eaacdd06a865b0f1f9a52e02785255f0aa7edb22
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-14 15:47:26 +0400

    Brand entity

---
+++ src/main/java/com/rent/entities/concretes/Brand.java
@@ -0,0 +1,14 @@
+package com.rent.entities.concretes;
+
+public class Brand {
+    private Long id;
+    private String name;
+
+    public Brand() {
+    }
+
+    public Brand(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
