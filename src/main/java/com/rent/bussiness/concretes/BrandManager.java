774f81cb51550f97bf1e3d76fb5893b3e6ae6c10
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-15 22:04:09 +0400

    updated

--- src/main/java/com/rent/bussiness/concretes/BrandManager.java
+++ src/main/java/com/rent/bussiness/concretes/BrandManager.java
@@ -1,12 +1,13 @@
 package com.rent.bussiness.concretes;
 
 import com.rent.bussiness.abstracts.BrandService;
+import com.rent.bussiness.responses.GetAllBrandsResponse;
 import com.rent.dataAccess.abstracts.BrandRepository;
 import com.rent.entities.concretes.Brand;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,7 +20,15 @@ public class BrandManager implements BrandService {
     }
 
     @Override
-    public List<Brand> getAll() {
-        return brandRepository.findAll();
+    public List<GetAllBrandsResponse> getAll() {
+        List<Brand> brands = brandRepository.findAll();
+        List<GetAllBrandsResponse> responseList = new ArrayList<>();
+        for (Brand brand : brands) {
+            GetAllBrandsResponse brandsResponse = new GetAllBrandsResponse();
+            brandsResponse.setId(brand.getId());
+            brandsResponse.setName(brand.getName());
+            responseList.add(brandsResponse);
+        }
+        return responseList;
     }
 }
