49b963935f4becb44bc28b63010f4a2b7eaa354a
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-15 22:35:27 +0400

    addBrand() added

--- src/main/java/com/rent/bussiness/abstracts/BrandService.java
+++ src/main/java/com/rent/bussiness/abstracts/BrandService.java
@@ -1,10 +1,12 @@
 package com.rent.bussiness.abstracts;
 
+import com.rent.bussiness.requests.CreateBrandRequest;
 import com.rent.bussiness.responses.GetAllBrandsResponse;
-import com.rent.entities.concretes.Brand;
 
 import java.util.List;
 
 public interface BrandService {
     List<GetAllBrandsResponse> getAll();
+
+   void addBrand(CreateBrandRequest createBrandRequest) ;
 }
