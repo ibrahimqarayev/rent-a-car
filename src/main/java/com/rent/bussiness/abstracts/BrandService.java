b158e60ce3147b83d8b918f12d4399f56f8b2e9e
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-15 22:04:00 +0400

    updated

--- src/main/java/com/rent/bussiness/abstracts/BrandService.java
+++ src/main/java/com/rent/bussiness/abstracts/BrandService.java
@@ -1,9 +1,10 @@
 package com.rent.bussiness.abstracts;
 
+import com.rent.bussiness.responses.GetAllBrandsResponse;
 import com.rent.entities.concretes.Brand;
 
 import java.util.List;
 
 public interface BrandService {
-    List<Brand> getAll();
+    List<GetAllBrandsResponse> getAll();
 }
